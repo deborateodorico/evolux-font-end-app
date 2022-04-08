cbd0ab8c36ade47aaa8a3c39b4af26706e75047b
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-08 15:36:28 -0300

    feat: create table model

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,32 +2,51 @@ import React from 'react';
 
 export default function Table() {
   return (
-    <table class='table'>
+    <table className='table'>
       <thead>
-        <tr>
+        <tr className='table-info'>
           <th scope='col'>#</th>
-          <th scope='col'>First</th>
-          <th scope='col'>Last</th>
-          <th scope='col'>Handle</th>
+          <th scope='col'>Number Available</th>
+          <th scope='col'>Monthy Price</th>
+          <th scope='col'>Setup Price</th>
+          <th scope='col'>Currency</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <th scope='row'>1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td>+55 (84) 91234-4321</td>
+          <td>0.03</td>
+          <td>3.40</td>
+          <td>U$</td>
         </tr>
         <tr>
           <th scope='row'>2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+          <td>+55 (84) 91234-4322</td>
+          <td>0.03</td>
+          <td>3.40</td>
+          <td>U$</td>
         </tr>
         <tr>
           <th scope='row'>3</th>
-          <td colspan='2'>Larry the Bird</td>
-          <td>@twitter</td>
+          <td>+55 (84) 91234-4323</td>
+          <td>0.03</td>
+          <td>3.40</td>
+          <td>U$</td>
+        </tr>
+        <tr>
+          <th scope='row'>4</th>
+          <td>+55 (84) 91234-4324</td>
+          <td>0.03</td>
+          <td>3.40</td>
+          <td>U$</td>
+        </tr>
+        <tr>
+          <th scope='row'>5</th>
+          <td>+55 (84) 91234-4325</td>
+          <td>0.03</td>
+          <td>3.40</td>
+          <td>U$</td>
         </tr>
       </tbody>
     </table>
