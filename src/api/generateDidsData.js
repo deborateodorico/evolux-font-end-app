42ae0fd407cfd8bc992310059df829421041226c
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-10 15:10:07 -0300

    feat: use randon function to generate values

--- src/api/generateDidsData.js
+++ src/api/generateDidsData.js
@@ -10,7 +10,13 @@ export default function generateDidsData(didsQuantity) {
   };
 
   for (let i = 1; i <= didsQuantity; i++) {
-    let newdObj = { ...obj, id: i };
+    let newdObj = {
+      ...obj,
+      id: i,
+      value: `+55 84 91234-432${i}`,
+      monthyPrice: Math.random().toFixed(2),
+      setupPrice: (Math.random() * 11).toFixed(2),
+    };
     dataArr.push(newdObj);
   }
   return dataArr;
