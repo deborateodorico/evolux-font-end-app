369d6bb2a2351ee9edc564f403be70870ee1cd27
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-11 14:22:02 -0300

    feat: add createAsyncThunk and extraReducers to slice function

--- src/redux/didsSlice.js
+++ src/redux/didsSlice.js
@@ -1,12 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchDids } from '../api';
+
+export const getDids = createAsyncThunk('dids/getDids', async () => {
+  return await fetchDids();
+});
+const initialState = {
+  didsList: [],
+  status: null,
+};
 
 export const slice = createSlice({
   name: 'dids',
-  initialState: [],
-
-  reducers: {
-    addNewDids(state, { payload }) {
-      return [...state, ...payload];
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getDids.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [getDids.fulfilled]: (state, { payload }) => {
+      state.didsList = payload;
+      state.status = 'success';
+    },
+    [getDids.rejected]: (state) => {
+      state.status = 'failed';
     },
   },
 });
