0e4979c9fe57878c5c5de0ee7adc22e400c11d2e
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-10 21:12:00 -0300

    feat: import fetchDids function and update de daids information using useState.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import DidsTable from './components/table/DidsTable';
 import { Layout } from './components/layout/Layout';
-import generateDidsData from './api/generateDidsData';
+import { fetchDids } from './api/';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const data = generateDidsData(5);
+  const [didsData, setDidsData] = useState(null);
+
+  async function fetchAllDids() {
+    const dids = await fetchDids();
+
+    setDidsData(dids);
+  }
+
+  useEffect(() => {
+    fetchAllDids();
+  }, []);
+
+  if (!didsData) return 'Loading...';
 
   return (
     <div className='app'>
       <Layout>
-        <DidsTable data={data} />
+        <DidsTable data={didsData} />
       </Layout>
     </div>
   );
