9b403b2774d11c6ce5a88df01ca74d3ed8e451f8
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-16 15:43:10 -0300

    feat: create updateDids folder and component. This component render a modal form

---
+++ src/components/UpdateDids/UpdateDids.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import HyperModal from 'react-hyper-modal';
+import styles from '../addDids/AddDidsButton.module.scss';
+import updateIcon from '../../icons/updateIcon.png';
+import ModalForm from '../form/ModalForm';
+import { updateDid } from '../../redux/didsSlice';
+
+export default function UpdateDids({
+  value,
+  monthyPrice,
+  setupPrice,
+  currency,
+  id,
+}) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleClickAddDidsButton = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const btnStyle = 'btn btn-secondary';
+
+  const updateTextButton = 'Update Did';
+
+  return (
+    <div>
+      <HyperModal
+        isOpen={modalIsOpen}
+        requestClose={handleCloseModal}
+        classes={{
+          wrapperClassName: styles['modal-wrapper'],
+          contentClassName: styles['content-wrapper'],
+        }}
+      >
+        <ModalForm
+          value={value}
+          monthyPrice={monthyPrice}
+          setupPrice={setupPrice}
+          currency={currency}
+          id={id}
+          onSubmit={updateDid}
+          btnStyle={btnStyle}
+          updateTextButton={updateTextButton}
+        />
+      </HyperModal>
+
+      <button
+        type='button'
+        onClick={handleClickAddDidsButton}
+        className={`btn btn-lg btn-sm ${styles['update-did-button']}`}
+      >
+        <img src={updateIcon} alt='update icon' />
+      </button>
+    </div>
+  );
+}
