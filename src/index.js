b4b49b4038b336f1656538045408dbf7bc05cd42
Débora Teodoríco <debora.steodorico@gmail.com>
2022-04-18 14:59:47 -0300

    feat: import App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
